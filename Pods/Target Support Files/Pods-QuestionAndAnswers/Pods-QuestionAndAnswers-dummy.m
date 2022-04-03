000a8ca993c88e9742152d2757fa347cd7912d55
WenJie Qiu <qiuwenjie1997@163.com>
2022-04-03 20:54:38 +0800

    init

---
+++ Pods/Target Support Files/Pods-QuestionAndAnswers/Pods-QuestionAndAnswers-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QuestionAndAnswers : NSObject
+@end
+@implementation PodsDummy_Pods_QuestionAndAnswers
+@end
---
+++ QuestionAndAnswers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuestionAndAnswers
+//
+//  Created by 澈水 on 2022/4/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
